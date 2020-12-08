aa8ab3ef8cbeeba81446edde9f06ddb7bd0e5e27
Matheus Ferreira <matheusfdantas03@gmail.com>
2020-12-08 17:55:11 -0300

    User: Upload Avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig);
 
 usersRoutes.post('/', async (request, response) => {
   try {
@@ -22,5 +27,27 @@ usersRoutes.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatarService = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatarService.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  },
+);
 
 export default usersRoutes;
